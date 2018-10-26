0af733fab8bb068546070ccb9189d8e746880b19
lei0904 <964358140@qq.com>
2018-10-26 15:08:27 +0800

    first commit

---
+++ start/json/menu.js
@@ -0,0 +1,41 @@
+{
+    "code": 0,
+    "msg": "",
+    "data": [{
+    "title": "主页"
+    ,"icon": "layui-icon-home"
+    ,"list": [
+        {
+            "title": "统计分析"
+            ,"jump": "/home/homePage"
+        },
+        {
+            "title": "客户管理"
+            ,"jump": "/manage/manage"
+        },
+        {
+            "title": "派单管理"
+            ,"jump": "/manage/manage"
+        },
+        {
+            "title": "任务单管理"
+            ,"jump": "/manage/manage"
+        },
+        {
+            "title": "服务单管理"
+            ,"jump": "/manage/manage"
+        },
+        {
+            "title": "报表管理"
+            ,"jump": "/manage/manage"
+        },
+        {
+            "title": "服务商管理"
+            ,"jump": "/manage/manage"
+        },
+        {
+            "title": "系统管理"
+            ,"jump": "/manage/manage"
+        }]
+    }]
+}
